d4022a4b5355900cea56b012e18eba7faed68fbb
unknown <sikar.samon@gmail.com>
2020-06-27 13:48:14 -0500

    left over question added

--- Question21.py
+++ Question21.py
@@ -2,3 +2,19 @@
 element in each tuple from a given list of non-empty tuples.
 Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]"""
 
+
+def sorted_tuple(a):
+    for i in range(0, len(a)-1):
+        for j in range(0, len(a)-1):
+            if a[j][1] > a[j+1][1]:
+                swap = a[j+1]
+                a[j+1] = a[j]
+                a[j] = swap
+                print(a, i)
+    return a
+
+
+print(sorted_tuple([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
+
+
+
